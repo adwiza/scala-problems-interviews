b27746bb6dd320485baa5f83a48885fee8f34295
Aleksei Elokhin <aleksei.e@clips4sale.com>
2023-08-16 07:06:09 +0300

    rockJVM scala lesson2 10:00

--- src/main/scala/com/rockthejvm/recap/ScalaRecap.scala
+++ src/main/scala/com/rockthejvm/recap/ScalaRecap.scala
@@ -44,7 +44,65 @@ object ScalaRecap extends App {
   // method notation
   val alice = Person("Alice", 23)
   val lamborghini = new Supercar
-  alice drives lamborghini // <-- infix notation
+  alice drives lamborghini // <-- infix notation alice.drives(lamborghini)
+
+  // generics
+  class MySet[+T]
+
+  // singletons & companions
+  object MySet
+
+  /**
+   * Functional programming
+   */
+
+    // functions are instances of the FunctionX trait
+  val incrementer = new Function1[Int, Int] {
+      override def apply(x: Int): Int = x + 1
+    }
+  val two = incrementer(1) // same as incrementer.apply(1)
+
+  // syntax sugars for functions
+  val anotherIncrementer = (x: Int) => x + 1 // same as new Function1[Int, Int] {...}
+
+  // higher-order functions
+  val incrementedList = List(1, 2, 3).map(incrementer) // function is passed as argument to the map method
+  //                                  ^^^ this is a NEW list
+
+  /**
+   * Collections
+   */
+  // list
+  val aList = List(1, 2, 3, 4)
+
+  // sequences = abstract representation of elements in a given order
+  val aSeq = Seq(1, 2, 3, 4) // Seq is a trait, so the Seq companion's apply() actually builds a list
+
+  // arrays
+  val anArray = Array.ofDim[Int](2, 3) // <- bi-directional in this call
+
+  // sets: every element appears once
+  val aSet = Set(1, 1, 2, 3)
+
+  // vectors: efficient Seq implementation
+  val aVector = Vector(1, 2, 3, 4)
+
+  // tuples
+  val aTuple: (Int, String) = (1, "I love tuples! Yau. Please bring more tuples.")
+
+  // maps
+  val aMap = Map(
+    "Daniel" -> 789,
+    "Jess" -> 555
+  )
+
+  /**
+   * Pattern matching
+   */
+
+
+
+
 
 
 }
