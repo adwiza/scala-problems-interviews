3ce309dec41fba7213f8e0c5d7608ba0b98ad2f3
Acid Wizard <adwiz@suroviy.ru>
2024-01-29 20:36:56 +0300

    rockJVM scala lesson37 0:00

--- src/main/scala/com/rockthejvm/graphs/GraphProblems.scala
+++ src/main/scala/com/rockthejvm/graphs/GraphProblems.scala
@@ -1,5 +1,7 @@
 package com.rockthejvm.graphs
 
+import scala.annotation.tailrec
+
 object GraphProblems extends App {
 
   type Graph[T] = Map[T, Set[T]]
@@ -12,6 +14,9 @@ object GraphProblems extends App {
     "Mary" -> Set("Bob", "Charlie")
   )
 
+  /**
+   * Easy problems
+   */
   // number of nodes this node `node` is associated or (adjacent) to
   def outDegree[T](graph: Graph[T], node: T): Int =
     if (graph.contains(node)) graph(node).size
@@ -23,5 +28,52 @@ object GraphProblems extends App {
 
   println(outDegree(socialNetwork, "Alice")) // 3
   println(outDegree(socialNetwork, "David")) // 2
+
+  /**
+   * Medium difficulty problems
+   */
+  def isPath[T](graph: Graph[T], start: T, end: T): Boolean = {
+    @tailrec
+    def isPathTailrec(remaining: List[T], consideredNodes: Set[T]): Boolean = {
+      if (remaining.isEmpty) false
+      else {
+        val node = remaining.head
+        if (node == end) true
+        else if (consideredNodes.contains(node)) isPathTailrec(remaining.tail, consideredNodes)
+        else isPathTailrec(remaining.tail ++ graph(node), consideredNodes + node)
+      }
+    }
+
+    isPathTailrec(List(start), Set())
+  }
+
+  println(isPath(socialNetwork, "Alice", "Mary")) // true
+  println(isPath(socialNetwork, "Bob", "Mary")) // false
+
+  def findPath[T](graph: Graph[T], start: T, end: T): List[T] = {
+    @tailrec
+    def findPathTailrec(remaining: List[(T, List[T])], consideredNodes: Set[T]): List[T] = {
+      if (remaining.isEmpty) List()
+      else {
+        val (node, currentPath) = remaining.head
+        if (node == end) currentPath.reverse
+        else if (consideredNodes.contains(node)) findPathTailrec(remaining.tail, consideredNodes)
+        else {
+          val neighbors = graph(node)
+          val tuples = neighbors.map(n => (n, n :: currentPath))
+          findPathTailrec(remaining.tail ++ tuples, consideredNodes + node)
+        }
+      }
+    }
+    findPathTailrec(graph(start).map(n => (n, n :: List(start))).toList, Set(start))
+  }
+
+  def findCycle[T](graph: Graph[T], node: T): List[T] = findPath(graph, node, node)
+
+  println(findPath(socialNetwork, "Charlie", "Mary"))
+  println(findPath(socialNetwork, "Alice", "Mary"))
+  println(findPath(socialNetwork, "Bob", "Mary"))
+  // test cycles
+  println(findCycle(socialNetwork, "Alice"))
 
 }
